2b05d2c76d06f193674eeb46082131883bb058f7
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-06-03 15:08:26 +0530

    done till building Navbar.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,8 @@
-
 "use client";
-
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { Button } from "@/components/ui/button";
-import ModeToggle from "@/components/ModeToggle";
-
 export default function Home() {
   return (
     <div>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-      <ModeToggle />
+      <h1>Home Page Content</h1>
     </div>
   );
 }
