048d1cab2c0370e56bee06f8fa9205b9daaef473
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-06-06 22:54:26 +0530

    changed app name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ async function Navbar() {
               href="/"
               className="text-xl font-bold text-primary font-mono tracking-wider"
             >
-              Ecoverse
+              TalkaSphere
             </Link>
           </div>
 
